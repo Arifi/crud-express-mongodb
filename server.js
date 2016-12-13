b9032b0bdc6edf0f9ad72316f7c9db511c8d2415
mourad <mourad.arifi@armediacg.com>
2016-12-13 19:45:02 +0100

    Using Express

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log('May Node be with you')
+console.log('May Node be with you');
+const express = require('express');
+const app = express();
+app.listen(3000, function() {
+  console.log('listening on 3000')
+});
