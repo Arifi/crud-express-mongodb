ebb8bd789d3c4abce4dfa917242005f7a23ce7c9
mourad <mourad.arifi@armediacg.com>
2016-12-13 19:53:48 +0100

    hello world

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const app = express();
 app.listen(3000, function() {
   console.log('listening on 3000')
 });
+
+// ES6 code (Arrow function)
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+/*
+app.get('/', function(req, res) {
+  res.send('Hello World')
+})
+// Note: request and response are usually written as req and res respectively.
+*/
