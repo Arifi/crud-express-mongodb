1044107290bc99d4e6697820eb4d7d3c8df2fef8
mourad <mourad.arifi@armediacg.com>
2016-12-13 23:32:23 +0100

    Use a template engine to display the quotes

--- server.js
+++ server.js
@@ -4,18 +4,20 @@ const bodyParser= require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
 
-var db;
-
+var db
+var port = 3000
 const mlablink = 'mongodb://mou:armed@ds133438.mlab.com:33438/crud-express-mongodb'
+
 
 MongoClient.connect(mlablink, (err, database) => {
   
   if (err) return console.log(err);
   db = database;
-  app.listen(3000, () => {
-    console.log('listening on 3000');
+  app.listen(port, () => {
+    console.log('listening on ' + port);
 	console.log('DIRNAME : ' + __dirname);
   })
   
@@ -23,13 +25,13 @@ MongoClient.connect(mlablink, (err, database) => {
 
 // ES6 code (Arrow function)
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-  var cursor = db.collection('quotes').find()
+  //var cursor = db.collection('quotes').find()
   //console.log(cursor)
-  db.collection('quotes').find().toArray(function(err, results) {
-	  console.log(results)
-	  // send HTML file populated with quotes here
-	  })
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
 })
 
 app.post('/quotes', (req, res) => {
