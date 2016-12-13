f416585db30625537e03edc1b7cc82a2cdf4d1d6
mourad <mourad.arifi@armediacg.com>
2016-12-13 19:57:53 +0100

    sendFile index.html

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ console.log('May Node be with you');
 const express = require('express');
 const app = express();
 app.listen(3000, function() {
-  console.log('listening on 3000')
+  console.log('listening on 3000');
+  console.log('DIRNAME : ' + __dirname);
 });
 
 // ES6 code (Arrow function)
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
 })
 
 /*
